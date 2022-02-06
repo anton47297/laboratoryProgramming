9662437a027696ad1e79cade6bf585c87a391b24
Anton_Bondarenko <bondarenkoanton442@gmail.com>
2022-02-06 15:09:45 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/untitled5b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5b.dir/main.c.obj"
+  "libuntitled5b.dll.a"
+  "untitled5b.exe"
+  "untitled5b.exe.manifest"
+  "untitled5b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
