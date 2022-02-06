a111cbd5ecaf404ef64ee2522e161908303639c6
Anton_Bondarenko <bondarenkoanton442@gmail.com>
2022-02-06 13:25:01 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/untitledLaba5b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitledLaba5b.dir/main.c.obj"
+  "libuntitledLaba5b.dll.a"
+  "untitledLaba5b.exe"
+  "untitledLaba5b.exe.manifest"
+  "untitledLaba5b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitledLaba5b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
